865580741ba500dcc4d2bec64344ab2c9519bb4d
Jim Yarbro <yarbro@outlook.com>
2016-10-12 08:00:53 +0200

    Added a menu
    Sometimes it's nice to only reset the sharepoint services. Other times you need to do a little more.

--- SPDevEnvReset.ps1
+++ SPDevEnvReset.ps1
@@ -1,5 +1,4 @@
 # Reset SharePoint development environment
-# github.com/jyarbro/SPDevEnvReset
 
 $CurrentPrincipal = [Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()
 $IsAdmin = ($CurrentPrincipal).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")
@@ -12,42 +11,72 @@ If (-NOT $IsAdmin) {
 	Break
 }
 
+function CompleteReset() {
+    Write-Host ""
+    Write-Host -foregroundcolor Red "Completely resetting dev environment"
+
+    net stop WAS /y
+
+    net stop SPTimerV4
+    net stop SPUserCodeV4
+
+
+    Write-Host ""
+    Write-Host -foregroundcolor Red "Ending Visual Studio SharePoint Host"
+    Write-Host ""
+
+    taskkill /im vssphost*
+
+    Write-Host ""
+    Write-Host -foregroundcolor Yellow "Deleting xml files from config cache"
+
+    $path = "C:\ProgramData\Microsoft\SharePoint\Config\*-*\*.xml"
+    Remove-Item -path $path -Force
+
+    Write-Host ""
+    Write-Host -foregroundcolor Yellow "Clearing timer cache"
+
+    $path = "C:\ProgramData\Microsoft\SharePoint\Config\*-*\cache.ini"
+    Set-Content -path $path -Value "1"
+
+    net start SPUserCodeV4
+    net start SPTimerV4
+
+    net start W3SVC
+}
+
+function ResetIIS() {
+    net stop WAS /y
+
+    net stop SPTimerV4
+    net stop SPUserCodeV4
+
+    net start SPUserCodeV4
+    net start SPTimerV4
+
+    net start W3SVC
+}
+
+function ResetSPServices() {
+    net stop SPTimerV4
+    net stop SPUserCodeV4
+
+    net start SPTimerV4
+    net start SPUserCodeV4
+}
+
+Write-Host "1: Reset SharePoint Services"
+Write-Host "2: Reset IIS & SharePoint Services"
+Write-Host "3: Complete development environment reset"
 Write-Host ""
-Write-Host -foregroundcolor White "Reset SharePoint development environment"
 
-Write-Host ""
-Write-Host -foregroundcolor Red "Stopping services"
-Write-Host ""
+$ProcessId = Read-Host -Prompt "Select a process (default 1): "
 
-net stop WAS /y
-net stop SPTimerV4
-net stop SPUserCodeV4
+Clear-Host
 
-Write-Host ""
-Write-Host -foregroundcolor Red "Ending Visual Studio SharePoint Host"
-Write-Host ""
-
-taskkill /im vssphost*
-
-Write-Host ""
-Write-Host -foregroundcolor Yellow "Deleting xml files from config cache"
-
-$path = "C:\ProgramData\Microsoft\SharePoint\Config\*-*\*.xml"
-Remove-Item -path $path -Force
-
-Write-Host ""
-Write-Host -foregroundcolor Yellow "Clearing timer cache"
-
-$path = "C:\ProgramData\Microsoft\SharePoint\Config\*-*\cache.ini"
-Set-Content -path $path -Value "1"
-
-Write-Host ""
-Write-Host -foregroundcolor Yellow "Starting services"
-Write-Host ""
-
-net start W3SVC
-net start SPTimerV4
-net start SPUserCodeV4
-
-Write-Host -foregroundcolor Green "Process complete"
-Write-Host ""
+switch($ProcessId) {
+    default { ResetSPServices }
+    "1" { ResetSPServices }
+    "2" { ResetIIS }
+    "3" { CompleteReset }
+}
